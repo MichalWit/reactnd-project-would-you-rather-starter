0f63c7208ea4b689e1cff7d94b0ff0cc1badf7e0
Michal <michalwit111@gmail.com>
2020-05-02 11:19:26 +0200

    func: display full score for a user in leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import ImageWithContent from './shared/ImageWithContent'
 
 class LeaderBoard extends Component {
 
     render() {
+        const { users } = this.props
+        const sortedUsersWithScores =
+            Object.values(users)
+                .map((user) => {
+                    const questions = user.questions.length
+                    const answers = Object.keys(user.answers).length
+                    const sum = questions + answers
+                    return {
+                        score: {
+                            sum,
+                            questions,
+                            answers,
+                        },
+                        user
+                    }
+                }
+                )
+                .sort((userA, userB) => (userB.score.sum - userA.score.sum))
         return (
-            <div>
-                <h2>Best result</h2>
-                <ul>
-                    <li>A</li>
-                    <li>B</li>
-                </ul>
-            </div>
+            <ul>
+                {
+                    sortedUsersWithScores.map(({score, user}) => (
+                        <li key={user.id}>
+                            <ImageWithContent
+                                name={user.name}
+                                avatarURL={user.avatarURL}
+                                label="asks"
+                            >
+                                <div>score: {score.sum}</div>
+                                <div>questions: {score.questions}</div>
+                                <div>answers: {score.answers}</div>
+                            </ImageWithContent>
+                        </li>
+                    ))
+                }
+            </ul>
         )
     }
 }
 
-export default LeaderBoard
+export default connect((store) => ({users: store.users}))(LeaderBoard)
